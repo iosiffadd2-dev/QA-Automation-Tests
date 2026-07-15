e8f494198d3ea2ba05022545825775f43eaa3f7a
iosiffadd2-dev <iosiffadd2@gmail.com>
2026-07-15 20:08:46 +0300

    chore: bootstrap Playwright test project

---
+++ tests/api/health.spec.ts
@@ -0,0 +1,10 @@
+import {expect, test} from '@playwright/test'
+
+test (' @smoke health status', async ({request}) => {
+    const response = await request.get('https://practice.expandtesting.com/notes/api/health-check')
+    expect(response.status()).toBe(200)
+    expect (await response.json()).toMatchObject({
+        success: true,
+        status: 200
+    })
+})

1cc714ce6843de6dca22ce7abe6c8e90fbc0f89f
iosiffadd2-dev <iosiffadd2@gmail.com>
2026-07-17 22:58:05 +0300

    first part of smoke tests added

---
+++ tests/ui/basics/add-remove-elements.spec.ts
@@ -0,0 +1,27 @@
+import { expect, test } from '@playwright/test';
+
+test('@smoke add and remove elements', async ({ page }) => {
+  await page.goto('/');
+  await page.getByRole('link', { name: 'Add/Remove Elements', exact: true }).click();
+  await expect(page).toHaveURL('/add-remove-elements');
+  await expect(
+    page.getByRole('heading', {
+      name: 'Add/Remove Elements page for Automation Testing Practice',
+    }),
+  ).toBeVisible();
+
+  const NumberOfClicks = 5;
+  const DeleteButtons = page.getByRole('button', { name: ' Delete', exact: true });
+  const AddElements = page.getByRole('button', { name: 'Add Element', exact: true });
+
+  await expect(DeleteButtons).toHaveCount(0);
+  for (let click = 0; click < NumberOfClicks; click++) {
+    await AddElements.click();
+  }
+
+  await expect(DeleteButtons).toHaveCount(NumberOfClicks);
+  for (let click = 0; click < NumberOfClicks; click++) {
+    await DeleteButtons.first().click();
+  }
+  await expect(DeleteButtons).toHaveCount(0);
+});
